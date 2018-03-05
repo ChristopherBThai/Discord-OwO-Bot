6a4e7539eedf00e58236118c7fe0c17714ceee72
ChristopherBThai <christopherbthai@gmail.com>
2018-03-05 09:59:43 -0800

    Fixed daily cowoncy buggit add .

--- methods/cowoncy.js
+++ methods/cowoncy.js
@@ -23,18 +23,18 @@ exports.display = function(con, client, msg){
  */
 exports.daily = function(con,msg){
 	var gain = 100 + Math.floor(Math.random()*100);
-	var sql = "SELECT TIMESTAMPDIFF(HOUR,daily,NOW()) AS hour,TIMESTAMPDIFF(MINUTE,daily,NOW()) AS minute,TIMESTAMPDIFF(SECOND,daily,NOW()) AS second FROM cowoncy WHERE id = "+msg.author.id+";UPDATE cowoncy SET daily = NOW(), money = money + "+gain+" WHERE id = "+msg.author.id+" AND TIMESTAMPDIFF(DAY,daily,NOW()) >= 1;";
+	var sql = "SELECT TIMESTAMPDIFF(HOUR,daily,NOW()) AS hour,TIMESTAMPDIFF(MINUTE,daily,NOW()) AS minute,TIMESTAMPDIFF(SECOND,daily,NOW()) AS second FROM cowoncy WHERE id = "+msg.author.id+" AND TIMESTAMPDIFF(DAY,daily,NOW())<1;"+
+		"INSERT INTO cowoncy (id,money) VALUES ("+msg.author.id+","+gain+") ON DUPLICATE KEY UPDATE money = IF(TIMESTAMPDIFF(DAY,daily,NOW()) >= 1,money+"+gain+",money), daily = IF(TIMESTAMPDIFF(DAY,daily,NOW()) >= 1,NOW(),daily);";
 	con.query(sql,function(err,rows,fields){
-		if(rows[0][0]==undefined)
-			msg.channel.send("*OwO* uh-oh! Something went wrong! I'll try to fix it as soon as I can!");
-		else if(rows[1].affectedRows==0){
+		console.log(rows);
+		if(rows[0][0]!=undefined){
 			var hour = 23 - rows[0][0].hour;
 			var min= 59 - (rows[0][0].minute%60);
 			var sec = 59 - (rows[0][0].second%60);
-			msg.channel.send("<:cowoncy:416043450337853441> Nu! "+msg.author.username+"! You need to wait **"+hour+" H "+min+" M "+sec+" S**")
+			msg.channel.send("**<:cowoncy:416043450337853441> Nu! "+msg.author.username+"! You need to wait __"+hour+" H "+min+" M "+sec+" S__**")
 				.then(message => message.delete(3000));
 		}else{
-			msg.channel.send("<:cowoncy:416043450337853441> *OwO What's this?*  Here's your daily **"+gain+"** Cowoncy, "+msg.author.username+"!");
+			msg.channel.send("**<:cowoncy:416043450337853441> *OwO What's this?*  Here's your daily __"+gain+" Cowoncy__, "+msg.author.username+"!**");
 		}
 	});
 }
