222d6af7a94431a482274f5efc26fcdef30d0fbf
christopherbthai <christopherbthai@gmail.com>
2018-09-01 23:37:51 +0000

    Updating drop/pickup commands

--- methods/command/gamble/drop.js
+++ methods/command/gamble/drop.js
@@ -6,13 +6,13 @@ module.exports = new CommandInterface({
 
 	args:"{amount}",
 
-	desc:"Drop some cowoncy in a channel with 'owo drop {amount}'! Users can pick it up with 'owo pickup'",
+	desc:"Drop some cowoncy in a channel with 'owo drop {amount}'! Users can pick it up with 'owo pickup {amount}'",
 
 	example:["owo drop 3000"],
 
 	related:[],
 
-	cooldown:60000,
+	cooldown:1000,
 	half:50,
 	six:300,
 	bot:true,
@@ -38,48 +38,44 @@ function drop(p){
 		return;
 	}
 	var sql = `SELECT money FROM cowoncy WHERE id = ${p.msg.author.id};
-		CALL CowoncyDrop(${p.msg.author.id},${p.msg.channel.id},${amount});
-		SELECT amount FROM cowoncydrop WHERE channel = ${p.msg.channel.id};`;
+		CALL CowoncyDrop(${p.msg.author.id},${p.msg.channel.id},${amount});`;
 	p.con.query(sql,function(err,result){
 		if(err){console.error(err);return;}
-		if(!result[2][0]){
-			p.send("**🚫 | "+p.msg.author.username+"**, an error has occured!! Oh uh...",3000);
-			return;
-		}
 		if(!result[0][0]||result[0][0].money<amount){
 			p.send("**🚫 | "+p.msg.author.username+"**, you don't have enough cowoncy! >:c",3000);
 			return;
 		}
-		var totalAmount = result[2][0].amount;
 		p.logger.value('cowoncy',(amount*-1),['command:drop','id:'+p.msg.author.id]);
-		p.send("**💰 | "+p.msg.author.username+"** dropped **"+p.global.toFancyNum(amount)+"** cowoncy! Use `owo pickup` to pick it up!\n**<:blank:427371936482328596> |** There is a total of **"+totalAmount+"** cowoncy in this channel!");
+		p.send("**💰 | "+p.msg.author.username+"** dropped **"+p.global.toFancyNum(amount)+"** cowoncy!\n**<:blank:427371936482328596> |** Use `owo pickup` to pick it up! ",8000);
 	});
 }
 
 function pickup(p){
+	var amount;
+	if(p.global.isInt(p.args[0])) amount = parseInt(p.args[0]);
+	if(!amount){
+		p.send("**🚫 | "+p.msg.author.username+"**, Please specify the pickup amount!",3000);
+		return;
+	}
+	if(amount<=0){
+		p.send("**🚫 | "+p.msg.author.username+"**, Invalid arguments!",3000);
+		return;
+	}
 	var sql = `SELECT amount FROM cowoncydrop WHERE channel = ${p.msg.channel.id};
-		CALL CowoncyPickup(${p.msg.author.id},${p.msg.channel.id});`;
+		SELECT money FROM cowoncy WHERE id = ${p.msg.author.id};
+		CALL CowoncyPickup(${p.msg.author.id},${p.msg.channel.id},${amount});`;
 	p.con.query(sql,function(err,result){
 		if(err){console.error(err);return;}
-		if(!result[0][0]||result[0][0].amount<=0){
-			sql = `SELECT money FROM cowoncy WHERE id = ${p.msg.author.id};
-				CALL CowoncyDropHundred(${p.msg.author.id},${p.msg.channel.id});`;
-			p.con.query(sql,function(err,result){
-				if(err){console.error(err);return;}
-				if(result[0][0]&&result[0][0].money>0){
-					var amount = result[0][0].money;
-					if(amount>100)
-						amount = 100;
-					p.logger.value('cowoncy',(amount*-1),['command:drop','id:'+p.msg.author.id]);
-					p.send("**🚫 | "+p.msg.author.username+"**! There is no cowoncy dropped on this channel!\n**<:blank:427371936482328596> |** You felt nice so you dropped **"+amount+"** cowoncy!");
-				}else{
-					p.send("**🚫 | "+p.msg.author.username+"**! There is no cowoncy dropped on this channel!");
-				}
-			});
-		}else{
-			var amount = result[0][0].amount;
+		//Not enough money
+		if(!result[1][0]||result[1][0].money<amount){
+			p.send("**🚫 | "+p.msg.author.username+"**, you can only pick up as much as you have!");
+			return;
+		}else if(result[0][0]&&amount <= result[0][0].amount&&amount<=result[1][0].money){
 			p.logger.value('cowoncy',amount,['command:drop','id:'+p.msg.author.id]);
 			p.send("**💰 | "+p.msg.author.username+"**, you picked up **"+amount+"** cowoncy from this channel!");
+		}else{
+			p.logger.value('cowoncy',(amount*-1),['command:drop','id:'+p.msg.author.id]);
+			p.send("**💰 | "+p.msg.author.username+"**, there's not enough cowoncy on the floor!\n**<:blank:427371936482328596> |** You felt nice so you dropped **"+amount+"** cowoncy!",8000);
 		}
 	});
 }
