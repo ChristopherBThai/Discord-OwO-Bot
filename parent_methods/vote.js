0257104aad2d239f71431d0bb00968161702bccf
Christopher Thai <christopherbthai@gmail.com>
2019-05-20 02:32:47 -0700

    Bug fixes

--- parent_methods/vote.js
+++ parent_methods/vote.js
@@ -32,7 +32,7 @@ function upvote(id,bot,json){
 	console.log("Webhooks for "+id);
 	var weekend = json?json.isWeekend:false;
 	var sql = "SELECT count,TIMESTAMPDIFF(HOUR,date,NOW()) AS time FROM vote WHERE id = "+id+";";
-	sql += "SELECT IF(patreonDaily = 1 OR ((TIMESTAMPDIFF(MONTH,patreonTimer,NOW())<patreonMonths) AND patreonType = 3),1,0) as patreon FROM user LEFT JOIN patreons ON user.uid = patreons.uid WHERE AND user.id = "+id+";";
+	sql += "SELECT IF(patreonDaily = 1 OR ((TIMESTAMPDIFF(MONTH,patreonTimer,NOW())<patreonMonths) AND patreonType = 3),1,0) as patreon FROM user LEFT JOIN patreons ON user.uid = patreons.uid WHERE user.id = "+id+";";
 	con.query(sql,function(err,result){
 		if(err) throw err;
 		var patreon = false;
