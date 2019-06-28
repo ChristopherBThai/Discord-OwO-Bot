d59bdc3c5b7c0050624da63bf51f286da2888985
Christopher Thai <christopherbthai@gmail.com>
2019-06-27 17:44:27 -0700

    Testing out some stuff

--- methods/command/admin/testcommand.js
+++ methods/command/admin/testcommand.js
@@ -10,36 +10,17 @@ const imagegen = require('../battle/battleImage.js');
 const patreon = require('../../../handler/patreonHandler.js');
 const mysql = require('../../../util/mysql.js');
 
+const captcha = require('../../../../tokens/captcha.js');
+
 module.exports = new CommandInterface({
 
-	alias:["testcommand"],
+	alias:["testcommand","tc"],
 
 	admin:true,
 
 	execute: async function(p){
-		console.log("testcommand");
-		//mysql.reconnect();
-		return;
-		/*
-		var sql = "SELECT COUNT(*) as count FROM animal;"
-		var result = await p.query(sql);
-		*/
-		var result = [{"count":600000}];
-		sql = "SELECT t.* FROM (SELECT id,name FROM animal LIMIT 1 OFFSET "+(Math.trunc(Math.random()*result[0].count))+") q JOIN animal t ON q.id = t.id AND q.name = t.name;";
-		sql += "SELECT t.* FROM (SELECT id,name FROM animal LIMIT 1 OFFSET "+(Math.trunc(Math.random()*result[0].count))+") q JOIN animal t ON q.id = t.id AND q.name = t.name;";
-		sql += "SELECT t.* FROM (SELECT id,name FROM animal LIMIT 1 OFFSET "+(Math.trunc(Math.random()*result[0].count))+") q JOIN animal t ON q.id = t.id AND q.name = t.name;";
-		result = await p.query(sql);
-		var player = [];
-		for(i=0;i<result.length;i++)
-			player.push(result[i][0])
-		var enemy = []
-		for(i=0;i<result.length;i++)
-			enemy.push(result[i][0])
-		try{
-			var image = await imagegen.generateImage(player,enemy);
-			p.send({file:image});
-		}catch(error){
-			console.error(error);
-		}
+		let data = await captcha();
+		p.send({files:[data.buffer]});
+		
 	}
 })
