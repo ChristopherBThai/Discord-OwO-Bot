6af5bf0710be3a3b2e6cbc9b8fbf105bd779b6c1
Christopher Thai <christopherbthai@gmail.com>
2019-06-27 19:19:29 -0700

    Fixed some security risks

--- methods/command/util/math.js
+++ methods/command/util/math.js
@@ -7,8 +7,10 @@
 
 const CommandInterface = require('../../commandinterface.js');
 
-const mathjs = require('mathjs');
 const mathEmoji = '<a:naruhodo:593971680737624065>';
+
+const workerpool = require('workerpool');
+const pool = workerpool.pool(__dirname+'/mathWorker.js');
 
 module.exports = new CommandInterface({
 
@@ -28,12 +30,33 @@ module.exports = new CommandInterface({
 	bot:true,
 
 	execute: function(p){
-		try{
-			let result = mathjs.evaluate(p.args.join(' '));
-			p.replyMsg(mathEmoji,", the answer is: **"+result+"**");
-		}catch(e){
-			p.errorMsg("... I don't think that's an equation silly head",3000);
-		}
+		pool.exec('compute',[p.args.join(" ")])
+			.timeout(1000)
+			.then(function(result){
+				if(result.mathjs){
+					if(result.data)
+						result = result.data+"";
+					else if(result.entries)
+						result = result.entries+"";
+					if(result.length>1000)
+						p.replyMsg(mathEmoji,", the answer is: **"+result.substr(0,1000)+"**...");
+					else
+						p.replyMsg(mathEmoji,", the answer is: **"+result+"**");
+				}else if(typeof result == 'object')
+					p.replyMsg(mathEmoji,", the answer is: **"+JSON.stringify(result)+"**");
+				else
+					p.replyMsg(mathEmoji,", the answer is: **"+result+"**");
+			}).catch(function(err){
+				if(err.message=='Promise timed out after 1000 ms')
+					p.errorMsg(", that equation is too difficult for me... :c",3000);
+				else
+					p.errorMsg("... I don't think that's an equation silly head",3000);
+			}).then(function(){
+				pool.terminate();
+			});
+
+
 	}
 
 })
+
---
+++ methods/command/util/mathWorker.js
@@ -0,0 +1,18 @@
+const workerpool = require('workerpool');
+const {create, all} = require('mathjs');
+const math = create(all);
+const limitedEvaluate = math.evaluate;
+math.import({
+	'import':     function () { throw new Error('Function import is disabled') },
+	'createUnit': function () { throw new Error('Function createUnit is disabled') },
+	'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+	'parse':      function () { throw new Error('Function parse is disabled') },
+	'simplify':   function () { throw new Error('Function simplify is disabled') },
+	'derivative': function () { throw new Error('Function derivative is disabled') }
+}, { override: true })
+
+function compute(expression){
+	return limitedEvaluate(expression);
+}
+
+workerpool.worker({compute:compute});
