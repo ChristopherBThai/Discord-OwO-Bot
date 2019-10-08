aec223936c28e69a27f5e7ab5e1f154909443f29
Christopher Thai <christopherbthai@gmail.com>
2019-10-07 19:20:36 -0700

    Added HSL options to random colors

--- methods/command/util/color.js
+++ methods/command/util/color.js
@@ -18,9 +18,9 @@ module.exports = new CommandInterface({
 
 	args:"{@user|RGB}",
 
-	desc:"Get a random color! You can also add an user argument or rgb value to view their colors!",
+	desc:"Get a random color! You can also add an user argument or rgb value to view their colors!\n\nIf you want to get real technical, you can adjust the saturation and light value of the random colors like in the example below!",
 
-	example:["owo color","owo color @user","owo color #FFFFFF","owo color 255,255,255"],
+	example:["owo color","owo color @user","owo color #FFFFFF","owo color 255,255,255","owo color S:50% L:50%"],
 
 	related:[],
 
@@ -57,6 +57,10 @@ module.exports = new CommandInterface({
 				color = parseMember(user);
 				title = ", here is the role color for **"+user.user.username+"**";
 
+			//randomHSL
+			}else if(p.args.join(" ").includes("%")){
+				color = randHSL(p,p.args);
+
 			//rgb values
 			}else if(args.length==3&&args.every(p.global.isInt)){
 				color = parseRGB(args);
@@ -85,8 +89,9 @@ module.exports = new CommandInterface({
 
 async function constructEmbed(color,p){
 	let description = "**HEX:** `"+color.hex+"`\n"
-						+"**R,G,B:** `"+color.rgb+"`\n"
-						+"**INT:** `"+color.intValue+"`";
+						+"**RGB:** `"+color.rgb+"`\n"
+						+"**INT:** `"+color.intValue+"`\n"
+						+"**HSL:** `"+color.hsl+"`";
 	let uuid = await generateImage(color);
 	let embed = {
 		description,
@@ -113,7 +118,9 @@ function parseIntValue(intValue){
 	let g = parseInt(hex.substring(2,4),16);
 	let b = parseInt(hex.substring(4,6),16);
 
-	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b};
+	let {hsl,h,s,l} = rgbToHsl(r,g,b);
+
+	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b,h,s,l,hsl};
 }
 
 function parseHex(hex){
@@ -123,7 +130,9 @@ function parseHex(hex){
 
 	let intValue = parseInt(hex.toLowerCase(),16);
 	
-	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b};
+	let {hsl,h,s,l} = rgbToHsl(r,g,b);
+
+	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b,h,s,l,hsl};
 }
 
 function parseRGB(rgb){
@@ -135,7 +144,9 @@ function parseRGB(rgb){
 
 	let intValue = parseInt(hex.toLowerCase(),16);
 
-	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b};
+	let {hsl,h,s,l} = rgbToHsl(r,g,b);
+
+	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b,h,s,l,hsl};
 }
 
 function randColor(){
@@ -147,7 +158,57 @@ function randColor(){
 
 	let intValue = parseInt(hex.toLowerCase(),16);
 
-	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b};
+	let {hsl,h,s,l} = rgbToHsl(r,g,b);
+
+	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b,h,s,l,hsl};
+}
+
+function randHSL(p,args){
+	let h,s,l;
+	let parsePercent = function(perc){
+		console.log(perc);
+		perc = parseFloat(perc.replace(/[HSVL%]/gi,""));
+		if(!perc) return -1;
+		perc /= 100;
+		if(perc>1) perc = 1;
+		if(perc<0) perc = 0;
+		return perc;
+	}
+	for(let i in args){
+		let arg = args[i].replace(/[:=]/gi,"").toUpperCase();
+		console.log(arg);
+		switch(arg.charAt(0)){
+			case 'H':
+				h = parsePercent(arg);
+				if(h==-1) return;
+				break;
+			case 'S':
+				s = parsePercent(arg);
+				if(s==-1) return;
+				break;
+			case 'V':
+				l = parsePercent(arg);
+				if(l==-1) return;
+				break;
+			case 'L':
+				l = parsePercent(arg);
+				if(l==-1) return;
+				break;
+			default:
+				return;
+		}
+
+	}
+
+	let goldenRatio = 0.618033988749895;
+	if(h===undefined) h = (Math.random()+goldenRatio)%1;
+	if(s===undefined) s = Math.random();
+	if(l===undefined) l = Math.random();
+	let {r,g,b} = hslToRgb(h,s,l);
+	let hex = toHex(r) + toHex(g) + toHex(b);
+	let intValue = parseInt(hex.toLowerCase(),16);
+	let hsl = Math.round(h*100)+"%, "+Math.round(s*100)+"%, "+Math.round(l*100)+"%";
+	return {r,g,b,hex:'#'+hex,intValue,rgb:r+','+g+','+b,h,s,l,hsl};
 }
 
 function toHex(num){
@@ -156,6 +217,52 @@ function toHex(num){
 		hex = "0"+hex;
 	}
 	return hex;
+}
+function rgbToHsl(r, g, b){
+	r /= 255, g /= 255, b /= 255;
+	let max = Math.max(r, g, b), min = Math.min(r, g, b);
+	let h, s, l = (max + min) / 2;
+
+	if(max == min){
+		h = s = 0; // achromatic
+	}else{
+		let d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch(max){
+			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / d + 2; break;
+			case b: h = (r - g) / d + 4; break;
+		}
+		h /= 6;
+	}
+
+	let hsl = Math.round(h*100)+"%, "+Math.round(s*100)+"%, "+Math.round(l*100)+"%";
+	return {h, s, l, hsl};
+}
+
+function hslToRgb(h, s, l){
+	let r, g, b;
+
+	if(s == 0){
+		r = g = b = l; // achromatic
+	}else{
+		let hue2rgb = function hue2rgb(p, q, t){
+			if(t < 0) t += 1;
+			if(t > 1) t -= 1;
+			if(t < 1/6) return p + (q - p) * 6 * t;
+			if(t < 1/2) return q;
+			if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+			return p;
+		}
+
+		let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		let p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1/3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1/3);
+	}
+
+	return {r:Math.round(r * 255), g:Math.round(g * 255), b:Math.round(b * 255)};
 }
 
 function generateImage(color){
