b5c2f08de3a6bc8b3d1766e6111a2d09fb2715c0
azmbekas <87832315+azmbekas@users.noreply.github.com>
2021-08-05 23:12:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const Discord = require(`discord.js`);
+const { Client, Collection, MessageEmbed,MessageAttachment } = require(`discord.js`);
+const { readdirSync } = require(`fs`);
+const { join } = require(`path`);
+const db = require('quick.db');
+const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
+const figlet = require("figlet");
+const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+client.login("ODcyODY5OTcyMjA3MTA4MTI2.YQwJgg.O8mmZOUI0vtJf3bY6Sht7_DfM24");
+client.commands = new Collection();
+client.setMaxListeners(0);
+client.prefix = PREFIX;
+client.queue = new Map();
+const cooldowns = new Collection();
+const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
+
+//this fires when the BOT STARTS DO NOT TOUCH
+
 /*
  * OwO Bot for Discord
  * Copyright (C) 2019 Christopher Thai
