2b00c84886a38eed1aa18968246966447079f4bc
chris thai <christopherbthai@gmail.com>
2021-08-20 00:31:45 -0700

    rogue console log

--- src/commands/commandList/patreon/chicken.js
+++ src/commands/commandList/patreon/chicken.js
@@ -90,7 +90,6 @@ async function give(p,user){
 	if (friendCount >= 7 && Math.random() <= 0.2) {
 		const friendCount = await p.redis.hincrby("data_"+user.id, data, -3);
 		const totalStolen = await p.redis.hincrby("data_custom", "chicken", 3);
-		console.log(totalStolen);
 		msg += `\n<a:jester1:878170294462853130> **|** ꒪꒳꒪psieee!  The OwO-Jester has stolen 3 chickens out of your inventory! <a:jester2:878170294756470795>`
 	}
 
