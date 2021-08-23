75c286e3890fb2ecb4fbbd83225a66ff97ce3430
Arche <66189242+arche-dev@users.noreply.github.com>
2021-08-23 02:33:42 +0000

    Reword rules.js
    Changed the wording since it uses a button instead of a reaction.

--- src/commands/commandList/utils/rules.js
+++ src/commands/commandList/utils/rules.js
@@ -49,7 +49,7 @@ module.exports = new CommandInterface({
 
 		/* Construct embed message */
 		let descriptionExtra = ""
-		if(!voted) descriptionExtra = "\n\n*Reacting with the emoji means you will follow the rules and acknowlege the consequences*";
+		if(!voted) descriptionExtra = "\n\n*Pressing the button below means you will follow the rules and acknowlege the consequences*";
 		else if(result[0][0].opinion == 1) descriptionExtra = "\n\nOwO what's this? You already agreed to these rules! <3";
 		else descriptionExtra = "\n\nUwU you disagreed! You still have to follow these rules though! c:<";
 		let embed = {
