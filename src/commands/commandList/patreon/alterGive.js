dca330d4efc898843cfb04bdae13063da558fc98
chris thai <christopherbthai@gmail.com>
2021-10-04 00:33:44 -0700

    patreon

--- src/commands/commandList/patreon/alterGive.js
+++ src/commands/commandList/patreon/alterGive.js
@@ -1,22 +1,24 @@
 /*
  * OwO Bot for Discord
- * Copyright (C) 2020 Christopher Thai
+ * Copyright (C) 2021 Christopher Thai
  * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
   */
 
 const blank = '<:blank:427371936482328596>';
 
-exports.alter = function(p, id, text, info){
-	switch(id){
+exports.alter = function (p, id, text, info) {
+	switch(id) {
 		case '456598711590715403':
 			return lexx(p, info);
+		case '605994815317999635':
+			return rhine(p, info);
 		default:
 			return text;
 	}
 }
 
-function lexx(p, info) {
+function lexx (p, info) {
 	const embed = {
 		color: p.config.embed_color,
 		image: {
@@ -25,6 +27,19 @@ function lexx(p, info) {
 		description: `💰 **|** **${info.from.username}** says: "Take My Money",`
 			+ `\n${blank} **|** sent **${info.amount}** cowoncy`
 			+ `\n${blank} **|** to **${info.to.username}**`
+	}
+	return { embed };
+}
+
+function rhine (p, info) {
+	const embed = {
+		color: 16758232,
+		image: {
+			url: "https://media.discordapp.net/attachments/887582241243422721/889401388633833472/lines-stars-757683.gif"
+		},
+		description: `💬 **|** ${info.from.username} says: "Save money and money will save you."`
+			+ `\n💵 **|** sent ${info.amount} cowoncy`
+			+ `\n🌸 **|** to ${info.to.username}`
 	}
 	return { embed };
 }
