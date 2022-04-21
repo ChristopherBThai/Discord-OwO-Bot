965fbc78699b8952aae62fb5dfdae3bcbcbe915b
unadjusted <85350984+unadjusted@users.noreply.github.com>
2022-04-21 12:31:48 +0200

    Update thorns.js

--- src/commands/commandList/battle/passives/thorns.js
+++ src/commands/commandList/battle/passives/thorns.js
@@ -18,7 +18,7 @@ module.exports = class Thorns extends PassiveInterface{
 		this.name = "Thorns";
 		this.basicDesc= "Reflect a percent of the damage dealt to you!";
 		this.emojis = ["<:cthorns:548729308030566426>","<:uthorns:548729400661639168>","<:rthorns:548729400628346900>","<:ethorns:548729400632410122>","<:mthorns:548729400607113216>","<:lthorns:548729400649187338>","<:fthorns:548729400468963329>"];
-		this.statDesc = "Reflect **?**% of the damage dealt to you as true damage";
+		this.statDesc = "Reflect **?%** of the damage dealt to you as true damage";
 		this.qualityList = [[15,35]];
 	}
 
