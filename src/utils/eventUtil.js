368a182569135ce48ba4883121f413d761c2c570
Christopher Thai <ChristopherBThai@gmail.com>
2024-02-13 20:47:10 -0800

    valentines event

--- src/utils/eventUtil.js
+++ src/utils/eventUtil.js
@@ -5,10 +5,9 @@
  * For more information, see README.md and LICENSE
  */
 const events = require('../data/event.json');
-const itemUtil = require('../commands/commandList/shop/util/itemUtil.js');
+//const itemUtil = require('../commands/commandList/shop/util/itemUtil.js');
 const rewardUtil = require('./rewardUtil.js');
 const lootboxUtil = require('../commands/commandList/zoo/lootboxUtil.js');
-const dailyMax = 5;
 const eventMax = 10;
 const itemToEvents = {};
 for (const key in events) {
@@ -67,15 +66,10 @@ exports.useItem = async function (item) {
 exports.getEventItem = async function () {
 	const event = getCurrentActive();
 	if (!event) return;
-	if (event?.type === 'halloween') {
-		//checkHalloween.bind(this)(event);
-		return;
-	} else if (event?.type === 'christmas') {
-		checkChristmas.bind(this)(event);
+	const random = Math.random();
+	if (random >= event.chance) {
 		return;
 	}
-	if (!event.item) return;
-
 	// Cache if user is done today
 	let today = new Date();
 	today = today.toLocaleDateString();
@@ -92,56 +86,39 @@ exports.getEventItem = async function () {
 	const con = await this.startTransaction();
 	let claimed;
 	try {
-		let rand = Math.random();
-		let sql = `SELECT * FROM user_item WHERE uid = ${uid} AND name = '${event.item.id}';`;
+		let sql = `SELECT * FROM user_event WHERE uid = ${uid} AND name = '${event.type}';`;
 		const result = await con.query(sql);
 		claimed = (result[0]?.claim_count || 0) + 1;
 
 		const reset = this.dateUtil.afterMidnight(result[0]?.claim_reset);
-		if (result[0] && result[0]?.claim_count >= dailyMax && !reset.after) {
+		if (result[0] && result[0]?.claim_count >= eventMax && !reset.after) {
 			this.msg.author.eventItemDone = today;
 			con.rollback();
 			return;
 		}
 		if (reset.after) {
-			rand = 0;
 			claimed = 1;
 		}
 
-		if (rand > event.item.chance) {
-			con.rollback();
-			return;
-		}
-
-		sql = `INSERT INTO user_item (uid, name, count, claim_reset, claim_count) VALUES 
-						(${uid}, '${event.item.id}', 1, ${reset.sql}, 1) ON DUPLICATE KEY UPDATE
-						count = count + 1, claim_count = ${claimed}, claim_reset = ${reset.sql};`;
+		sql = `INSERT INTO user_event (uid, name, claim_reset, claim_count) VALUES 
+						(${uid}, '${event.type}', ${reset.sql}, 1) ON DUPLICATE KEY UPDATE
+						claim_count = ${claimed}, claim_reset = ${reset.sql};`;
 		await con.query(sql);
+
+		const { rewardSql, rewardTxt, rewardEmoji } = await getEventRewards.bind(this)(this.msg.author);
+		await con.query(rewardSql);
+		this.send(`${rewardEmoji} **|** \`[${claimed}/${eventMax}]\` ${rewardTxt}`);
+
 		await con.commit();
 	} catch (err) {
 		console.error(err);
 		con.rollback();
 		return;
 	}
-
-	const item = itemUtil.getByName(event.item.id);
-	const text =
-		event.item.foundText.replaceAll('?emoji?', event.item.emoji) +
-		` \`[${claimed}/${dailyMax}]\`\n${this.config.emoji.blank} **|** To use this item, type \`owo use ${item.id}\``;
-
-	this.send(text);
 };
 
 exports.isValentines = function () {
-	// TODO remove next year
-	if (Date.now() < 1676448000000) {
-		return false;
-	}
-	return isEventActive(events['valentine']);
-};
-
-exports.getValentines = function () {
-	return events['valentine'];
+	return getCurrentActive()?.type === 'valentine';
 };
 
 function isEventActive(event) {
@@ -215,83 +192,21 @@ async function parseReward(reward, con) {
 	return rewardUtil.getReward(this.msg.author.id, uid, con, reward.type, reward.id, reward.count);
 }
 
-async function checkChristmas(event) {
-	const random = Math.random();
-	if (random >= event.chance) {
-		return;
-	}
-	// Cache if user is done today
-	let today = new Date();
-	today = today.toLocaleDateString();
-	if (this.msg.author.eventItemDone) {
-		const date = this.msg.author.eventItemDone;
-		if (date === today) {
-			return;
-		} else {
-			delete this.msg.author.eventItemDone;
-		}
-	}
-	const uid = await this.global.getUserUid(this.msg.author);
-
-	const con = await this.startTransaction();
-	let claimed;
-	try {
-		let sql = `SELECT * FROM user_event WHERE uid = ${uid} AND name = 'christmas';`;
-		const result = await con.query(sql);
-		claimed = (result[0]?.claim_count || 0) + 1;
-
-		const reset = this.dateUtil.afterMidnight(result[0]?.claim_reset);
-		if (result[0] && result[0]?.claim_count >= eventMax && !reset.after) {
-			this.msg.author.eventItemDone = today;
-			con.rollback();
-			return;
-		}
-		if (reset.after) {
-			claimed = 1;
-		}
-
-		sql = `INSERT INTO user_event (uid, name, claim_reset, claim_count) VALUES 
-						(${uid}, 'christmas', ${reset.sql}, 1) ON DUPLICATE KEY UPDATE
-						claim_count = ${claimed}, claim_reset = ${reset.sql};`;
-		await con.query(sql);
-
-		const { rewardSql, rewardTxt } = await getChristmasRewards.bind(this)(this.msg.author);
-		const candies = [
-			'<:bulb1:1188727159309733888>',
-			'<:bulb2:1188727160433811456>',
-			'<:bulb3:1188727161411088485>',
-			'<:bulb4:1188727157434888243>',
-		];
-		const emoji = candies[Math.floor(Math.random() * candies.length)];
-		await con.query(rewardSql);
-		this.send(`${emoji} **|** \`[${claimed}/${eventMax}]\` ${rewardTxt}`);
-
-		await con.commit();
-	} catch (err) {
-		console.error(err);
-		con.rollback();
-		return;
-	}
-
-	/*
-		event.item.foundText.replaceAll('?emoji?', event.item.emoji) +
-		` \`[${claimed}/${dailyMax}]\`\n${this.config.emoji.blank} **|** To use this item, type \`owo use ${item.id}\``;
-		*/
-}
-
-async function getChristmasRewards(user) {
+async function getEventRewards(user) {
 	const id = user.id;
 	const uid = await this.global.getUserUid(user);
 	let rand = Math.random();
+	rand = 1.4;
 
 	if (rand <= 0.15) {
 		// Cowoncy
 		let rewardCount = 1000;
 		rewardCount = Math.floor(rewardCount + Math.random() * 4000);
 		return {
-			rewardTxt: `You take a peek under the tree. What's this? You found **${this.global.toFancyNum(
-				rewardCount
-			)} ${this.config.emoji.cowoncy} Cowoncies**!`,
+			rewardTxt: `You received a red envelope! It has **${this.global.toFancyNum(rewardCount)} ${
+				this.config.emoji.cowoncy
+			} Cowoncies**, that's dope!`,
+			rewardEmoji: '🧧',
 			rewardSql: `INSERT INTO cowoncy (id,money) VALUES (${id}, ${rewardCount}) ON DUPLICATE KEY UPDATE money = money + ${rewardCount};`,
 		};
 	} else if (rand <= 0.3) {
@@ -299,35 +214,47 @@ async function getChristmasRewards(user) {
 		let rewardCount = 300;
 		rewardCount = Math.floor(rewardCount + Math.random() * 700);
 		return {
-			rewardTxt: `Oops! You knocked an ornament off the tree and found **${this.global.toFancyNum(
+			rewardTxt: `OwO what's this? A black cat left a gift behind? It's **${this.global.toFancyNum(
 				rewardCount
-			)} ${this.config.emoji.shards} Weapon Shards** inside!`,
+			)} ${this.config.emoji.shards} Weapon Shards** to make your weapons refined!`,
+			rewardEmoji: '🐈‍⬛',
 			rewardSql: `INSERT INTO shards (uid,count) VALUES (${uid},${rewardCount}) ON DUPLICATE KEY UPDATE count = count + ${rewardCount};`,
 		};
 	} else if (rand <= 0.45) {
 		// Lootbox
 		let rewardCount = 1;
 		rewardCount = Math.floor(rewardCount + Math.random() * 2);
+		let rewardTxt = `Happy Chinese New Year! I hope `;
+		if (rewardCount > 1) {
+			rewardTxt += `these **${rewardCount} ${this.config.emoji.lootbox} Lootboxes** can make your luck appear!`;
+		} else {
+			rewardTxt += `this **${rewardCount} ${this.config.emoji.lootbox} Lootbox** can make your luck appear!`;
+		}
 		return {
-			rewardTxt: `Wait, these aren’t boxes of decorations. Are these gems? You find **${rewardCount} ${
-				this.config.emoji.lootbox
-			} Lootbox${rewardCount > 1 ? 'es' : ''}**!`,
+			rewardTxt,
+			rewardEmoji: `🧧`,
 			rewardSql: `INSERT INTO lootbox (id,boxcount,claimcount,claim) VALUES (${id},${rewardCount},0,'2017-01-01') ON DUPLICATE KEY UPDATE boxcount = boxcount + ${rewardCount};`,
 		};
 	} else if (rand <= 0.6) {
 		// Crate
 		let rewardCount = 1;
 		rewardCount = Math.floor(rewardCount + Math.random() * 2);
+		let rewardTxt = `Look! A fairy with a gift for you! She left behind **${rewardCount} ${this.config.emoji.crate} Weapon Crate`;
+		if (rewardCount > 1) {
+			rewardTxt += `s**, they look brand new!`;
+		} else {
+			rewardTxt += `**, it looks brand new!`;
+		}
 		return {
-			rewardTxt: `While putting the bauble on the tree, you notice **${rewardCount} ${
-				this.config.emoji.crate
-			} Weapon Crate${rewardCount > 1 ? 's' : ''}** hanging off the branches!`,
+			rewardTxt,
+			rewardEmoji: '🧚',
 			rewardSql: `INSERT INTO crate (uid,cratetype,boxcount,claimcount,claim) VALUES (${uid},0,${rewardCount},0,'2017-01-01') ON DUPLICATE KEY UPDATE boxcount = boxcount + ${rewardCount};`,
 		};
 	} else if (rand <= 0.75) {
 		// Cookie
 		return {
-			rewardTxt: `Huh- who decided to put a **${this.config.emoji.cookie} Cookie** on top of the tree? You take the cookie and replace it with a star.`,
+			rewardTxt: `Happy Valentine's! Enjoy this nice treat. I hope this **${this.config.emoji.cookie} Cookie** is very very sweet!`,
+			rewardEmoji: '❤️',
 			rewardSql: `INSERT INTO rep (id, count) VALUES (${id},1) ON DUPLICATE KEY UPDATE count = count + 1;`,
 		};
 	} else if (rand <= 0.9) {
@@ -338,16 +265,16 @@ async function getChristmasRewards(user) {
 		let gemSql = gem.sql;
 		gem = Object.values(gem.gems)[0].gem;
 		return {
-			rewardTxt: `What's this shiny star on top of the tree? Woah, you found ${this.global.getA(
-				gem.rank
-			)} **${gem.emoji} ${gem.rank} ${gem.type} Gem**!`,
+			rewardTxt: `OwO! I wish you Good luck in the year ahead. If not, this **${gem.emoji} ${gem.rank} ${gem.type} Gem** will give you fortune instead!`,
+			rewardEmoji: '🧧',
 			rewardSql: gemSql,
 		};
 	} else {
 		// Special Pet
-		let animal = this.global.validAnimal('2023christmas_owo');
+		let animal = this.global.validAnimal('2024feb_owo');
 		return {
-			rewardTxt: `You notice some movement behind the tree... OwO? What's this? It's ${animal.value} OwO trying to hiding from you!`,
+			rewardTxt: `A cutie appeared, she asks, "Will you be my Valentine?". ${animal.value} OwO wants to stay with you all the time!`,
+			rewardEmoji: '💌',
 			rewardSql: `INSERT INTO animal (count, totalcount, id, name) VALUES (1,1,${id},'${animal.value}')
 					ON DUPLICATE KEY UPDATE count = count + 1, totalcount = totalcount + 1;
 					INSERT INTO animal_count (id, ${animal.rank}) VALUES (${id}, 1)
